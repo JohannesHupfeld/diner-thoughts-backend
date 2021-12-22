4f947f5969709e1126396e0dacd902e6d3539c7b
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-21 22:34:14 -0500

    aded location id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_014105) do
+ActiveRecord::Schema.define(version: 2021_12_22_025916) do
 
   create_table "locations", force: :cascade do |t|
     t.string "city"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_12_22_014105) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "location_id"
   end
 
 end
